2d926c28ab54a514ed1d4beba5aef6b1bd9372af
MENETRIER Baptiste <baptiste.menetrier@ecole-navale.fr>
2023-12-19 16:28:29 +0100

    :sparkles: pulse train

--- signals.py
+++ signals.py
@@ -15,6 +15,30 @@ def pulse(T, f, fs):
     return s, t
 
 
+def pulse_train(T, f, fs, interpulse_delay=None):
+    """Generate train of pulses"""
+    pulse_duration = 4 / f
+    if interpulse_delay is None:
+        interpulse_delay = 0.5 * pulse_duration
+
+    omega = 2 * np.pi * f
+    t_train = np.arange(0, T, 1 / fs)
+    s_train = np.zeros(len(t_train))
+    nb_motif = int(np.ceil(T / (interpulse_delay + pulse_duration)))
+    for i in range(nb_motif):
+        t_pulse = t_train - i * (interpulse_delay + pulse_duration)
+        s_pulse = np.zeros(len(t_pulse))
+
+        idx_tpulse = np.logical_and(0 < t_pulse, t_pulse < pulse_duration)
+        t_pulse = t_pulse[idx_tpulse]
+        s_pulse[idx_tpulse] = (
+            1 / 2 * np.sin(omega * t_pulse) * (1 - np.cos(1 / 4 * omega * t_pulse))
+        )
+        s_train += s_pulse
+
+    return s_train, t_train
+
+
 def sine_wave(f0, fs, T, A, phi):
     if fs < 2 * f0:
         raise ValueError(

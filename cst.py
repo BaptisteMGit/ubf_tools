4860e03b5160cb2b51491246f0e546a2bb94afc5
MENETRIER Baptiste <baptiste.menetrier@ecole-navale.fr>
2024-01-11 08:44:14 +0100

    Define colors for event and library features

--- cst.py
+++ cst.py
@@ -3,7 +3,7 @@ from colorama import Fore
 """ Usefull constants """
 
 
-# Physical constants
+# Physical properties
 C0 = 1500  # Sound celerity in water (m/s)
 RHO_W = 1000  # Water density (kg/m3)
 SAND_PROPERTIES = {
@@ -13,10 +13,13 @@ SAND_PROPERTIES = {
     "a_p": 0.8,  # Compression wave attenuation (dB/wavelength)
     "a_s": 2.5,  # Shear wave attenuation (dB/wavelength)
 }  # Sand properties from Jensen et al. (2000) p.39
-# Default parameters
+
+
+# Verlinden parameters
 MAX_LOC_DISTANCE = 50 * 1e3  # Maximum localisation distance (m)
 AVERAGE_LOC_DISTANCE = 10 * 1e3  # Average localisation distance (m)
-
+LIBRARY_COLOR = "blue"
+EVENT_COLOR = "black"
 
 # TDQM bar format
 BAR_FORMAT = "%s{l_bar}%s{bar}%s{r_bar}%s" % (

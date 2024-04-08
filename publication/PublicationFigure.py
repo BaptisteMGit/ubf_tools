c9ce82a1831f7add50b7e9249b45ec93dc6c5083
MENETRIER Baptiste <baptiste.menetrier@ecole-navale.fr>
2024-04-08 12:03:13 +0200

    Add params to control constrained layout

--- publication/PublicationFigure.py
+++ publication/PublicationFigure.py
@@ -15,7 +15,15 @@ class PubFigure:
         suplabel_fontsize=22,
         titlepad=20,
         labelpad=15,
+        pad_inches=10,
         dpi=100,
+        fmt="png",
+        subplot_hspace=0.1,
+        subplot_wspace=0.1,
+        constrained_layout_h_pad=0.15,
+        constrained_layout_w_pad=0.15,
+        constrained_layout_hspace=0.1,
+        constrained_layout_wspace=0.1,
     ):
         self.size = size
         self.label_fontsize = label_fontsize
@@ -25,23 +33,41 @@ class PubFigure:
         self.suplabel_fontsize = suplabel_fontsize
         self.titlepad = titlepad
         self.labelpad = labelpad
-        self.dpi = dpi
-        self.set_all_fontsize()
+        self.pad_inches = pad_inches
 
-    def apply_ticks_fontsize(self):
-        plt.xticks(fontsize=self.ticks_fontsize)
-        plt.yticks(fontsize=self.ticks_fontsize)
+        # Constrained layout
+        self.constrained_layout_h_pad = constrained_layout_h_pad
+        self.constrained_layout_w_pad = constrained_layout_w_pad
+        self.constrained_layout_hspace = constrained_layout_hspace
+        self.constrained_layout_wspace = constrained_layout_wspace
+
+        # Unconstrained subplot params
+        self.subplot_hspace = subplot_hspace
+        self.subplot_wspace = subplot_wspace
+
+        self.dpi = dpi
+        self.fmt = fmt
+        self.set_all_params()
 
     def set_full_screen(self):
         mpl.rcParams["figure.max_open_warning"] = 0
         mng = plt.get_current_fig_manager()
         mng.window.state("zoomed")
 
-    def set_all_fontsize(self):
+    def set_all_params(self):
         params = {
             "legend.fontsize": self.legend_fontsize,
             "figure.figsize": self.size,
             "figure.titlesize": self.title_fontsize,
+            "figure.labelsize": self.label_fontsize,
+            "figure.dpi": self.dpi,
+            "figure.subplot.hspace": self.subplot_hspace,
+            "figure.subplot.wspace": self.subplot_wspace,
+            "figure.constrained_layout.use": True,
+            "figure.constrained_layout.hspace": self.constrained_layout_hspace,
+            "figure.constrained_layout.wspace": self.constrained_layout_wspace,
+            "figure.constrained_layout.h_pad": self.constrained_layout_h_pad,
+            "figure.constrained_layout.w_pad": self.constrained_layout_w_pad,
             "axes.labelsize": self.label_fontsize,
             "axes.titlesize": self.title_fontsize,
             "xtick.labelsize": self.ticks_fontsize,

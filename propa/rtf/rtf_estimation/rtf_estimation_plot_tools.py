179c9ea048e0b53ae554636c508996268fcb580a
Baptiste Menetrier <baptiste.menetrier@ecole-navale.fr>
2024-10-24 16:03:10 +0200

    Add fct to compare hermitian angle to transmitted energy

--- propa/rtf/rtf_estimation/rtf_estimation_plot_tools.py
+++ propa/rtf/rtf_estimation/rtf_estimation_plot_tools.py
@@ -18,18 +18,29 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+from propa.rtf.ideal_waveguide import print_arrivals
 from propa.rtf.rtf_estimation.rtf_estimation_utils import *
 from propa.rtf.rtf_estimation.rtf_estimation_const import *
+from propa.rtf.rtf_utils import (
+    D_frobenius,
+    D_hermitian_angle,
+    true_rtf,
+    interp_true_rtf,
+)
+
 
 # ======================================================================================================================
 # Plot tools
 # ======================================================================================================================
 
 
-def plot_ir(kraken_data, shift_ir=True):
+def plot_ir(kraken_data, shift_ir=True, plot_arrivals=True):
     plt.figure()
     t = kraken_data["t"]
     delta_rcv = 500
+
+    depth, r_src, z_src, z_rcv, _ = waveguide_params()
+    arrivals = print_arrivals(z_src=z_src, z_rcv=z_rcv, r=r_src, depth=depth, n=4)
 
     if shift_ir:
         xlims = [19, 24]
@@ -38,6 +49,29 @@ def plot_ir(kraken_data, shift_ir=True):
         xlims = [16, 24]
         fname = "kraken_ir"
 
+    title = waveguide_info_line()
+
+    if plot_arrivals:
+        for k in range(arrivals.shape[0]):
+            plt.axvline(
+                arrivals[k, 0],
+                color="k",
+                linestyle="--",
+                linewidth=1,
+            )
+            plt.text(
+                arrivals[k, 0] - 0.1,
+                np.max(kraken_data[f"rcv{0}"]["ir"]) * 0.75,
+                r"$t_{\textrm{arr}"
+                + f"{k+1}"
+                + r"} ="
+                + f"{np.round(arrivals[k, 0], 2)}"
+                + r"\, \textrm{s}$",
+                rotation=90,
+                color="k",
+                fontsize=12,
+            )
+
     for i in range(kraken_data["n_rcv"]):
         ir = kraken_data[f"rcv{i}"]["ir"]
         if shift_ir:
@@ -45,11 +79,11 @@ def plot_ir(kraken_data, shift_ir=True):
             shift = int(tau / (t[1] - t[0]))
             ir = np.roll(ir, shift)
 
-        plt.plot(t, ir, label=f"rcv{i}")
+        plt.plot(t, ir, label=r"$rcv_{" + f"{i}" + r"}$")
 
     plt.xlabel(r"$t \, \textrm{[s]}$")
     plt.ylabel(r"$h(t)$")
-    plt.title("Kraken impulse response")
+    plt.title(title)
     plt.grid()
     plt.xlim(xlims)
     plt.legend()
@@ -60,19 +94,21 @@ def plot_tf(kraken_data):
     plt.figure()
     for i in range(kraken_data["n_rcv"]):
         plt.plot(
-            kraken_data["f"], np.abs(kraken_data[f"rcv{i}"]["h_f"]), label=f"rcv{i}"
+            kraken_data["f"],
+            np.abs(kraken_data[f"rcv{i}"]["h_f"]),
+            label=r"$rcv_{" + f"{i}" + r"}$",
         )
     plt.xlabel(r"$f \, \textrm{[Hz]}$")
     plt.ylabel(r"$|H(f)|$")
-    plt.title("Kraken transfert function")
+    plt.title(waveguide_info_line())
     plt.grid()
     plt.xlim([0, 50])
     plt.legend()
     plt.savefig(os.path.join(ROOT_DATA, "kraken_tf.png"))
 
 
-def plot_signal():
-    rcv_sig = derive_received_signal()
+def plot_signal(tau_ir):
+    rcv_sig = derive_received_signal(tau_ir=tau_ir)
     t = rcv_sig["t"]
     idx_window_center = int(len(t) / 2)
 
@@ -254,6 +290,18 @@ def csdm_info_line(fig_props):
     return info
 
 
+def waveguide_info_line():
+    depth, r_src, z_src, z_rcv, _ = waveguide_params()
+
+    p0 = r"$r_{\textrm{src}} = " + str(r_src * 1e-3) + r"\, \textrm{km},\quad$"
+    p1 = r"$z_{\textrm{src}} = " + str(z_src) + r"\, \textrm{m},\quad$"
+    p2 = r"$z_{\textrm{rcv}} = " + str(z_rcv) + r"\, \textrm{m},\quad$"
+    p3 = r"$D = " + str(depth) + r"\, \textrm{m}$"
+    info = p0 + p1 + p2 + p3
+
+    return info
+
+
 def plot_rcv_signals(fig_props, t, rcv_sig, rcv_noise, rcv_idx_to_plot=0):
     # Plot signal, noise and signal + noise in time domain for the first receiver (subplots)
     rcv_sig_0 = rcv_sig[:, rcv_idx_to_plot]
@@ -313,7 +361,8 @@ def plot_rcv_stfts(fig_props, t, rcv_sig, rcv_noise, rcv_idx_to_plot=0):
     # Plot stft
     # nperseg = 2**12
     # noverlap = int(nperseg * 3 / 4)
-    nperseg = 2**8
+    nperseg = 2**7
+    # noverlap = int(0.8 * nperseg)
     noverlap = int(0.8 * nperseg)
 
     fs = 1 / (t[1] - t[0])
@@ -442,6 +491,65 @@ def plot_signal_components(fig_props, t, rcv_sig, rcv_noise, rcv_idx_to_plot=0):
     plot_rcv_signals(fig_props, t, rcv_sig, rcv_noise, rcv_idx_to_plot=rcv_idx_to_plot)
     plot_rcv_stfts(fig_props, t, rcv_sig, rcv_noise, rcv_idx_to_plot=rcv_idx_to_plot)
     plot_rcv_autocorr(fig_props, t, rcv_sig, rcv_noise, rcv_idx_to_plot=rcv_idx_to_plot)
+
+
+def compare_rtf_vs_received_spectrum(
+    fig_props, f_cs, rtf_cs, f_cw=None, rtf_cw=None, rcv_signal=None
+):
+
+    # Load true RTF
+    kraken_data = load_data()
+    f_true, rtf_true = interp_true_rtf(kraken_data, f_cs)
+
+    # Derive Hermitian angle distance
+    dist_cs = D_hermitian_angle(rtf_ref=rtf_true, rtf=rtf_cs, unit="deg")
+    dist_cw = D_hermitian_angle(rtf_ref=rtf_true, rtf=rtf_cw, unit="deg")
+
+    # Plot Hermitian angle
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+
+    # Twin axis for received spectrum
+    spec2 = np.abs(rcv_signal[f"rcv{0}"]["spect"]) ** 2
+    val = -10 * np.log10(spec2)
+    f = rcv_signal["f"]
+
+    ax1.plot(f, val, color="k", linestyle="-", linewidth=0.2)
+    # ax1.scatter(f, val, color="k", marker=".", s=1)
+    ax1.set_ylabel(r"$-10 log_{10}(|S(f)|^2) \, \textrm{[dB]}$")
+
+    ax2.plot(
+        f_cs,
+        dist_cs,
+        label=r"$\theta_{\textrm{CS}}$",
+        linestyle="-",
+        color="b",
+        marker="o",
+        linewidth=0.2,
+        markersize=2,
+    )
+    ax2.plot(
+        f_cw,
+        dist_cw,
+        label=r"$\theta_{\textrm{CW}}$",
+        linestyle="-",
+        color="r",
+        marker="o",
+        linewidth=0.2,
+        markersize=2,
+    )
+    ax2.set_xlabel(r"$f \, \textrm{[Hz]}$")
+    ax2.set_ylabel(r"$\theta \, \textrm{[°]}$")
+
+    ax2.legend()
+    plt.title(
+        # r"$\textrm{RTF estimation with unpropagated white noise}$" +
+        f"\n({csdm_info_line(fig_props)})"
+    )
+
+    plt.savefig(
+        os.path.join(fig_props["folder_path"], f"hermitian_angle_vs_spectrum.png")
+    )
 
 
 def plot_rtf_estimation(fig_props, f_cs, rtf_cs, f_cw=None, rtf_cw=None):
@@ -603,15 +711,6 @@ def plot_rtf_estimation(fig_props, f_cs, rtf_cs, f_cw=None, rtf_cw=None):
         plt.savefig(
             os.path.join(fig_props["folder_path"], f"rtf_estimation_smooth_rcv{i}.png")
         )
-
-
-def true_rtf(kraken_data):
-    tf_ref = kraken_data[f"rcv{0}"]["h_f"]
-    rtf = np.zeros((len(kraken_data["f"]), kraken_data["n_rcv"]), dtype=complex)
-    for i in range(kraken_data["n_rcv"]):
-        rtf[:, i] = kraken_data[f"rcv{i}"]["h_f"] / tf_ref
-
-    return kraken_data["f"], rtf
 
 
 if __name__ == "__main__":

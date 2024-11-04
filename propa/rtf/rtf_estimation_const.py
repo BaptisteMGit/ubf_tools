a46c2ff3f42603d86c5983d5b9215ed9c6f61da9
Baptiste Menetrier <baptiste.menetrier@ecole-navale.fr>
2024-11-04 13:50:34 +0100

    Usefull constants

---
+++ propa/rtf/rtf_estimation_const.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+"""
+@File    :   rtf_estimation_const.py
+@Time    :   2024/10/18 11:12:55
+@Author  :   Menetrier Baptiste 
+@Version :   1.0
+@Contact :   baptiste.menetrier@ecole-navale.fr
+@Desc    :   None
+"""
+
+# ======================================================================================================================
+# Import
+# ======================================================================================================================
+import os
+from cst import RHO_W, C0
+
+ROOT_FOLDER = r"C:\Users\baptiste.menetrier\Desktop\devPy\phd\propa\rtf\rtf_estimation\short_ri_waveguide"
+ROOT_DATA = os.path.join(ROOT_FOLDER, "data")
+ROOT_RTF_DATA = (
+    r"C:\Users\baptiste.menetrier\Desktop\devPy\phd\data\rtf\ideal_waveguide"
+)
+
+N_RCV = 5  # Number of receivers

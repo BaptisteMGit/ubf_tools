1453918ad807c44e35ac55be5ef9dced8235af32
Baptiste Menetrier <baptiste.menetrier@ecole-navale.fr>
2025-05-05 14:25:05 +0200

    Sand properties added

--- source/global_constants.py
+++ source/global_constants.py
@@ -28,10 +28,10 @@ diagonal_loading = (
 p0 = 1e-6  # reference pressure in Pa
 c0 = 1500  # sound celerity in water (m/s)
 rho_w = 1000  # water density (kg/m3)
-# sand_properties = {
-#     "rho": 1.9 * rho_w,
-#     "c_p": 1650,  # P-wave celerity (m/s)
-#     "c_s": 0.0,  # S-wave celerity (m/s) TODO check and update
-#     "a_p": 0.8,  # Compression wave attenuation (dB/wavelength)
-#     "a_s": 2.5,
-# }  # Shear wave attenuation (dB/wavelength) # Sand properties from Jensen et al. (2000) p.39
+sand_properties = {
+    "rho": 1.9 * rho_w * 1e-3,  # Sand density (g/cm3)
+    "c_p": 1650,  # P-wave celerity (m/s)
+    "c_s": 0.0,  # S-wave celerity (m/s) TODO check and update
+    "a_p": 0.8,  # Compression wave attenuation (dB/wavelength)
+    "a_s": 2.5,
+}  # Shear wave attenuation (dB/wavelength) # Sand properties from Jensen et al. (2000) p.39

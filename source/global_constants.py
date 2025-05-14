aed8254bb049602c95767424dd5b9815690fa332
MENETRIER Baptiste <baptiste.menetrier@ecole-navale.fr>
2025-05-14 10:50:34 +0200

    Add sediment props

--- source/global_constants.py
+++ source/global_constants.py
@@ -28,10 +28,59 @@ diagonal_loading = (
 p0 = 1e-6  # reference pressure in Pa
 c0 = 1500  # sound celerity in water (m/s)
 rho_w = 1000  # water density (kg/m3)
+
+### Sediment properties ###
+# Sand properties from Jensen et al. (2000) p.39
 sand_properties = {
     "rho": 1.9 * rho_w * 1e-3,  # Sand density (g/cm3)
     "c_p": 1650,  # P-wave celerity (m/s)
     "c_s": 0.0,  # S-wave celerity (m/s) TODO check and update
     "a_p": 0.8,  # Compression wave attenuation (dB/wavelength)
     "a_s": 2.5,
-}  # Shear wave attenuation (dB/wavelength) # Sand properties from Jensen et al. (2000) p.39
+}  # Shear wave attenuation (dB/wavelength)
+
+"""
+Regarding sediment properties, the classification is based on the Folk 7-class system from the European Marine Observation and Data Network (EMODnet) :
+https://www.emodnet-geology.eu/map-viewer/?p=seabed_substrate
+
+The associated values were derived from a table provided in the appendix of the attached document (p.57). This appendix is from Quiet-Oceans, the company where I interned a few months before the project. According to the document, the values are sourced from:
+[19] EMODnet, 2012: Seabed substrate data (version 28.6.2012) made available by the EMODnet (European Marine Observation and Data Network) Geology project, funded by the European Commission Directorate General for Maritime Affairs and Fisheries. EMODnet Geology.
+"""
+boulders_bedrock_properties = {
+    "rho": 2.5,  # Sand density (g/cm3)
+    "c_p": 3820,  # P-wave celerity (m/s)
+    "c_s": 0.0,  # S-wave celerity (m/s) TODO check and update
+    "a_p": 0.75,  # Compression wave attenuation (dB/wavelength)
+    "a_s": 0.0,  # Shear wave attenuation (dB/wavelength)
+}
+
+coarse_sediment_properties = {
+    "rho": 2.37,  # Sand density (g/cm3)
+    "c_p": 2122,  # P-wave celerity (m/s)
+    "c_s": 0.0,  # S-wave celerity (m/s) TODO check and update
+    "a_p": 0.88,  # Compression wave attenuation (dB/wavelength)
+    "a_s": 0.0,  # Shear wave attenuation (dB/wavelength)
+}
+
+mixed_sediment_properties = {
+    "rho": 2.03,  # Sand density (g/cm3)
+    "c_p": 1855,  # P-wave celerity (m/s)
+    "c_s": 0.0,  # S-wave celerity (m/s) TODO check and update
+    "a_p": 0.89,  # Compression wave attenuation (dB/wavelength)
+    "a_s": 0.0,  # Shear wave attenuation (dB/wavelength)
+}
+muddy_sand_sand_properties = {
+    "rho": 1.53,  # Sand density (g/cm3)
+    "c_p": 1708,  # P-wave celerity (m/s)
+    "c_s": 0.0,  # S-wave celerity (m/s) TODO check and update
+    "a_p": 0.91,  # Compression wave attenuation (dB/wavelength)
+    "a_s": 0.0,  # Shear wave attenuation (dB/wavelength)
+}
+
+mud_sand_sandy_mud_properties = {
+    "rho": 1.16,  # Sand density (g/cm3)
+    "c_p": 1517,  # P-wave celerity (m/s)
+    "c_s": 0.0,  # S-wave celerity (m/s) TODO check and update
+    "a_p": 0.37,  # Compression wave attenuation (dB/wavelength)
+    "a_s": 0.0,  # Shear wave attenuation (dB/wavelength)
+}
